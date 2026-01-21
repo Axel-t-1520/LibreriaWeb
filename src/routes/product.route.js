ed9830af310c7b06c2a82c2e27ecc9ee8f5ff47f
Alex_T <alextancara4@gmail.com>
2026-01-21 18:01:02 -0400

    security

--- src/routes/product.route.js
+++ src/routes/product.route.js
@@ -10,15 +10,16 @@ import {
  
 } from "../controllers/products.controller.js";
 import upload from "../middleware/upload.middleware.js";
+import { verificarToken, verificarVendedor } from "../middleware/auth.middleware.js";
 
 const router = Router();
 
-router.get("/product", getProd);
-router.get("/product/categoria/:cat", getProductCat);
+router.get("/product", verificarToken,verificarVendedor,getProd);
+router.get("/product/categoria/:cat",verificarToken,verificarVendedor, getProductCat);
 router.get("/product/:id", getProductId);
-router.post("/regisProd",upload.single('imagen'), registerProduct);
-router.put('/product/update/:id', updateProd)
-router.delete('/product/delete/:id',deleteProd)
+router.post("/regisProd",verificarToken,upload.single('imagen'), registerProduct);
+router.put('/product/update/:id',verificarToken,verificarVendedor, updateProd)
+router.delete('/product/delete/:id',verificarToken,deleteProd)
 router.get('/productosTotal',productTotal)
 
 export default router;
